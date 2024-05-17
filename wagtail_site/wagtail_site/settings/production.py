357c8615fc2a8e2d3fffbac9e60abcd0b3a17780
Chapin Bryce <mail@chapinb.com>
2024-05-17 12:03:32 -0400

    F - Add settings for production

--- wagtail_site/wagtail_site/settings/production.py
+++ wagtail_site/wagtail_site/settings/production.py
@@ -1,7 +1,24 @@
 from .base import *  # noqa: F403
 from decouple import config
 
+SILENCED_SYSTEM_CHECKS = [
+    # security.W005, SECURE_HSTS_PRELOAD
+    # We are not ready to submit the site to the HSTS preload list,
+    # as it is in development
+    "security.W021",
+]
+
 DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("DJANGO_SECRET")
+
+SECURE_HSTS_SECONDS = 60  # Set to 1 minute while prototyping
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+ALLOWED_HOSTS = ["ssn-demo.chapinb.com"]
