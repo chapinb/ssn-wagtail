1c730cc2430c68411002c8a033dc13515c2eb58c
Chapin Bryce <mail@chapinb.com>
2024-05-17 13:17:42 -0400

    F - Add logging

--- wagtail_site/wagtail_site/settings/base.py
+++ wagtail_site/wagtail_site/settings/base.py
@@ -156,6 +156,20 @@ STORAGES = {
     },
 }
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
+
 
 # Wagtail settings
 
