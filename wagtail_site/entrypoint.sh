396d366c15860006305f773d66b2c1c75eb41791
Chapin Bryce <mail@chapinb.com>
2024-05-17 13:35:07 -0400

    F - Update entrypoint

---
+++ wagtail_site/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+set -e
+
+poetry run python manage.py collectstatic --noinput
+poetry run python manage.py migrate
+
+# For Production:
+poetry run gunicorn wagtail_site.wsgi:application --bind=0.0.0.0:8000 --log-file=-
