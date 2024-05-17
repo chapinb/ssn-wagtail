b0a39ee19e70eb0abee30a1526870fd025230ee1
Chapin Bryce <mail@chapinb.com>
2024-05-17 12:38:34 -0400

    F - Remove default value from settings

--- wagtail_site/wagtail_site/settings/dev.py
+++ wagtail_site/wagtail_site/settings/dev.py
@@ -2,7 +2,7 @@ from .base import *  # noqa: F403
 from decouple import config
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("DJANGO_SECRET", "development")
+SECRET_KEY = config("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
