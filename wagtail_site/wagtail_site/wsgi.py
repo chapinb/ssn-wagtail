7b874e750fc68752b75110b7c62d1a6ebcf44585
Chapin Bryce <mail@chapinb.com>
2024-05-17 13:03:19 -0400

    F - Remove assumed settings module

--- wagtail_site/wagtail_site/wsgi.py
+++ wagtail_site/wagtail_site/wsgi.py
@@ -7,10 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
-import os
-
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wagtail_site.settings.dev")
 
 application = get_wsgi_application()
